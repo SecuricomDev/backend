55e0bfb863b2352e37bb65fbaa0206ef3dac1d80
Cameron <cameronluyt30@gmail.com>
2023-09-14 13:42:48 +0200

    init

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ const router = require('./routes'); // Import your routes module
 
 const port = process.env.PORT || 3000;
 const dbURI = process.env.MONGODB_URI || 'mongodb+srv://CameronLuyt69:21102001@contact-keeper.hvfzf.mongodb.net/?retryWrites=true&w=majority'; // Use environment variables
-const corsOptions = {
-    origin: 'https://securicom-sms.netlify.app', // Replace with your Angular app's origin
+
+  const corsOptions = {
+    origin: function (origin, callback) {
+      // Check if the origin is allowed; you can add your logic here
+      // For development, you can allow all origins using a wildcard '*'
+      if (!origin || origin === 'https://securicom-sms.netlify.app') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'POST', // Allow only POST requests
   };
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
